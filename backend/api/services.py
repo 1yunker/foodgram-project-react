5732420577c98a50a122d343c59456fe2d840d99
Yuri Pomazkin <1yunker@gmail.com>
2023-08-10 00:06:59 +0500

    Update services

--- backend/api/services.py
+++ backend/api/services.py
@@ -8,10 +8,15 @@ def generate_shopping_list(user):
 
     # Берем список рецептов пользователя из корзины
     recipes = user.in_shopping_cart.values('recipe')
-    ingredients = RecipeIngredient.objects.filter(recipe__in=recipes).values(
+    ingredients = RecipeIngredient.objects.filter(
+        recipe__in=recipes
+    ).values(
         name=F('ingredient__name'),
         m_unit=F('ingredient__measurement_unit__name')
-    ).annotate(amount=Sum('amount')).order_by('name')
+    ).annotate(
+        amount=Sum('amount')
+    ).order_by('name')
+
     shopping_list = ('Список покупок:\n\n')
     shopping_list += '\n'.join([
         f'{ingredient["name"]} ({ingredient["m_unit"]}) - '
